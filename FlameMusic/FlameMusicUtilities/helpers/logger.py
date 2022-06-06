1af2802f181ccbd441b51336f28c7fe4680256cb
Khiladi King <79006624+Kishoth-45@users.noreply.github.com>
2022-06-05 21:47:05 -0700

    Add files via upload

---
+++ FlameMusic/FlameMusicUtilities/helpers/logger.py
@@ -0,0 +1,13 @@
+from FlameMusic.config import LOG_GROUP_ID
+from FlameMusic import app
+
+
+async def LOG_CHAT(message, what):
+    if message.chat.username:
+        chatusername = (f"@{message.chat.username}")
+    else:
+        chatusername = ("Private Group")
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    mention = "["+user_name+"](tg://user?id="+str(user_id)+")" 
+    
